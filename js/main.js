8c25a26c3e5e80c21ef7a05ffbbe8d236602b41a
Olga Lee <olyalee100@gmail.com>
2021-02-07 15:15:30 +0300

    Сгенерировала временные данные

--- js/main.js
+++ js/main.js
@@ -10,20 +10,96 @@ const getRandomInteger = (min, max) => {
   }
 
   if (min >= max) {
-    throw new Error(errorMessage + ' Верхнее значение диапазона должно быть больше нижнего.' );
+    throw new Error(errorMessage + min + max + ' Верхнее значение диапазона должно быть больше нижнего.' );
   }
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-
 const getRandomFloat = (min, max, lengthOfTail) => {
-  getRandomInteger(min, max);
-
-  min = Math.ceil(min * Math.pow(10, lengthOfTail));
-  max = Math.round(max * Math.pow(10, lengthOfTail));
+  min = Math.floor(min * Math.pow(10, lengthOfTail));
+  max = Math.ceil(max * Math.pow(10, lengthOfTail));
 
   const randomNumber = getRandomInteger(min, max);
   return randomNumber / Math.pow(10, lengthOfTail);
 }
 
-getRandomFloat();
+const TITLES = [
+  'Просторные апартаменты около парка',
+  'Уютная квартира в самом эпицентре ночных развлечений',
+  'Славный дом у озера',
+  'Квартира с потрясающим видом',
+  'Апартаменты в стиле декаданс',
+];
+const TYPES = ['palace', 'flat', 'house', 'bungalow'];
+const CHECKS = ['12:00', '13:00', '14:00'];
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const DESCRIPTION = [
+  'Имеется все необходимое для комфортного проживания: холодильник, кухня, зубные щетки бонусом',
+  'Чисто, стильно, комфортно. Всегда есть горячая вода',
+];
+const PHOTOS = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
+];
+
+const getRandomArrayElement = (array) => {
+  return array[getRandomInteger(0, array.length - 1)];
+};
+
+const getArrayOfRandomLength = (array) => {
+  return array.filter(() => Boolean(getRandomInteger(0, array.length)));
+};
+
+const generateRentalOffer = () => {
+  const avatar = 'img/avatars/user' + 0 + getRandomInteger(1, 8) + '.png';
+  const title = getRandomArrayElement(TITLES);
+  const price = getRandomInteger(0, 1000000);
+  const type = getRandomArrayElement(TYPES);
+  const rooms = getRandomInteger(0, 100);
+  const guests = getRandomInteger(0, 100);
+  const checkin = getRandomArrayElement(CHECKS);
+  const checkout = getRandomArrayElement(CHECKS);
+  const features = getArrayOfRandomLength(FEATURES);
+  const description = getRandomArrayElement(DESCRIPTION);
+  const photos = getArrayOfRandomLength(PHOTOS);
+  const getLocation = ({x, y}) => {
+    return (x + ', ' + y);
+  };
+  const location = {
+    x: getRandomFloat(35.65, 35.70, 5),
+    y: getRandomFloat(139.70, 139.80, 5),
+  };
+  const adress = getLocation(location);
+
+  return {
+    author: {
+      avatar: avatar,
+    },
+    offer: {
+      title: title,
+      adress: adress,
+      price: price,
+      type: type,
+      rooms: rooms,
+      guests: guests,
+      checkin: checkin,
+      checkout: checkout,
+      features: features,
+      description: description,
+      photos: photos,
+    },
+    location: location,
+  }
+}
+
+const createRentalOfferArray = () => {
+  let result = new Array();
+  for (let i = 0; i < 10; i++) {
+    const rentalOffer = generateRentalOffer(i);
+    result.push(rentalOffer);
+  }
+  return result;
+};
+
+createRentalOfferArray();
